0508c52d4a08396441b91c0fac06f93c3fbfc63e
Ricardo Alberco <sabado17_08_2024@outlook.com>
2025-07-24 00:55:02 -0500

    Fix: Se corrigió el error de build y se generó la versión de producción

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Asegura que Vercel sepa dónde están los archivos
-  },
-  base: '/', // Usa '/' si estás desplegando en dominio raíz
 })
